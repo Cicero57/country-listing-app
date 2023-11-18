d995548574853bc7d8d2e5be8b20ba52878e52a5
Cicero57 <eliteworld57@gmail.com>
2023-11-18 15:10:19 +0100

    style: updated styling

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div>
-      <footer className="bg-gray-800 text-white p-4">
+      <footer className="bg-gray-800 text-white p-4 text-center">
         <div className="container mx-auto">
-          <p>&copy; {currentYear} Okoye Victor. All rights reserved.</p>
+          <p>&copy; {currentYear} Made with &#x1F496;</p>
         </div>
       </footer>
     </div>
